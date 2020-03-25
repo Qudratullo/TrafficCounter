bff7d5325963e724834aac93a6065062c5b9f6be
Qudratullo <ibadullayev.qudrat@mail.ru>
2020-03-25 08:20:04 +0300

    Initial project

---
+++ src/main/java/com/example/trafficcounter/TrafficCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.trafficcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trafficcounter/TrafficCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trafficcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
